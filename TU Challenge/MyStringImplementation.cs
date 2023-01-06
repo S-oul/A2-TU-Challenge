e1091f51d30e1568c5e0f004d6356b4aac0b757d
Soul <S.soulSlasher@gmail.com>
2023-01-06 16:07:11 +0100

    Strings pas fini rip

--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -21,32 +21,104 @@ namespace TU_Challenge
 
         public static string MixString(string a, string b)
         {
-            throw new NotImplementedException();
+            string mixedString = "";
+
+            int maxLength = Math.Max(a.Length, b.Length);
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < a.Length)
+                {
+                    mixedString += b[i];
+                }
+                if (i < b.Length)
+                {
+                    mixedString += b[i];
+                }
+            }
+
+            return mixedString;
         }
 
         public static string ToLowerCase(string a)
         {
-            throw new NotImplementedException();
-        }
-
-        public static string Voyelles(string a)
-        {
-            throw new NotImplementedException();
+            string loweredStr = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                //go use le tolower de Char, si j'ai pas le string.tolower
+                loweredStr += Char.ToLower(a[i]);
+            }
+            return loweredStr;
         }
 
         public static string BazardString(string input)
         {
-            throw new NotImplementedException();
+            string skippedString = "";
+            string save = "";
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                skippedString += input[i];
+            }
+            for (int i = 1; i < input.Length; i += 2)
+            { 
+                save += input[i];
+            }
+                return skippedString +save;
         }
 
         public static string UnBazardString(string input)
         {
-            throw new NotImplementedException();
+            string reversedString = "";
+            string save = "";
+            for (int i = input.Length - 1; i >= 0; i -= 2)
+            {
+                reversedString += input[i];
+            }
+            for (int i = input.Length - 2; i >= 0; i -= 2)
+            {
+                save += input[i];
+            }
+        
+            return reversedString + save;
+        
         }
 
         public static string ToCesarCode(string input, int offset)
         {
-            throw new NotImplementedException();
+            {
+                string encryptedString = "";
+                foreach (char c in input)
+                {
+                    encryptedString += (char)(c + offset);
+                }
+                return encryptedString;
+            }
+        }
+
+        public static string Voyelles(string a)
+        {
+            string vowels = "";
+            a = ToLowerCase(a);
+            foreach (char c in a)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+                {
+                    vowels += c;
+                }
+            }
+
+            return vowels;
+        }
+
+        public static string Reverse(string a)
+        {
+            {
+                string reversed = "";
+                for (int i = a.Length - 1; i >= 0; i--)
+                {
+                    reversed += a[i];
+                }
+                return reversed;
+            }
         }
     }
 }
