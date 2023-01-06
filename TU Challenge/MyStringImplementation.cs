f100434d7aa8e083dcf1895568b638eaa416ac80
Sacha (Pc) <S.soulslasher@gmail.com>
2023-01-06 12:26:24 +0100

    debut string

--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -8,6 +8,45 @@ namespace TU_Challenge
 {
     public class MyStringImplementation
     {
-        
+        public static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            if (input == null || input == "" || input.Trim() == "")
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public static string MixString(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToLowerCase(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Voyelles(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
